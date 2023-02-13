897e11447ef473f2e6dc90d527e36a698b664404
Cajun <cajun_ray@qq.com>
2023-02-13 13:27:05 +0800

    添加头像；更新css样式

---
+++ config.js
@@ -0,0 +1,25 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: '雷雷の监测面板',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2011870-cf4a29da7f41671332eb52d2',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: false,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '博客',
+      url: 'https://ll.sc.cn/'
+    },
+  ],
+};
